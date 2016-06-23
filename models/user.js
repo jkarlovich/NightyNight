a2a77e6ba0430d5eb1c481541e769e1b140e98a4
Jamie Karlovich <jamierkarlovich@gmail.com>
2016-06-23 14:39:18 -0700

    user model tested for security

--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 'use strict';
+
+var bcrypt = require('bcrypt');
+
 module.exports = function(sequelize, DataTypes) {
   var user = sequelize.define('user', {
     email: {
@@ -13,7 +16,7 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING,
       validate: {
         len: {
-          args: [8,99],
+          args: [8, 99],
           msg: 'Password must be between 8 and 99 characters'
         }
       }
@@ -26,11 +29,30 @@ module.exports = function(sequelize, DataTypes) {
           msg: 'Name must be between 1 and 254 characters'
         }
       }
-    },
+    }
   }, {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
+      }
+    },
+    instanceMethods: {
+      validPassword: function(password) {
+        return bcrypt.compareSync(password, this.password);
+      },
+      toJSON: function() {
+        var jsonUser = this.get();
+        delete jsonUser.password;
+
+        return jsonUser;
+      }
+    },
+    hooks: {
+      beforeCreate: function(createdUser, options, cb) {
+        // hash password and save hash to user
+        var hash = bcrypt.hashSync(createdUser.password, 10);
+        createdUser.password = hash;
+        cb(null, user);
       }
     }
   });
