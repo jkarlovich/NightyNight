68cd41941f19140495276a9ada1890c238e5d508
Jamie Karlovich <jamierkarlovich@gmail.com>
2016-06-29 16:19:14 -0700

    delete button works

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
-  $('.delete').click(function (e) {
-    e.preventdefault();
+  $('.delete').click(function(e) {
+    e.preventDefault();
     var url = $(this).attr('href');
     var element = $(this);
     $.ajax({
       method: 'DELETE',
       url: url
     }).done(function(data) {
-      console.log(data.msg);
+      console.log(data);
       window.location='/profile';
     });
   });
